a6dbd4f09aa747c7175a3bc2e861cc4cdcee8360
Sachin Jain <sachinjain024@gmail.com>
2017-04-16 07:18:35 +0530

    Copying rules page content to web.requestly.in

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+/**
+ *  @References
+ * [0] http://gruntjs.com/getting-started to install grunt-cli
+ * [1]: https://github.com/01org/grunt-zipup
+ * [2]: https://github.com/gruntjs/grunt-contrib-handlebars
+ * [3]: http://gruntjs.com/configuring-tasks#files
+ **/
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          'css/main.css': 'sass/main.scss'
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['**/*.scss'],
+        tasks: ['sass']
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 7070,
+          hostname: 'localhost',
+          keepalive: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.registerTask('dev', ['watch']);
+  grunt.registerTask('libs', ['sass']);
+  grunt.registerTask('serve', ['libs', 'connect']);
+};
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# requestly-web
+# Purpose
+
+We are maintaining this site since some of Requestly users are still using v4.1.4 and due to their use cases they have disabled further updates.
+They have requested me to maintain web.requestly.in for sometime until they get the updates.
